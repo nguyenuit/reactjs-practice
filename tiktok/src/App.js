9382c2f4d64816e5b49de10c6e3345f0f10e29de
nguyen <nguyen@digital-launch.com>
2023-01-03 14:07:15 +0700

    update useState, re-cal total

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,10 +1,13 @@
 import { useState } from "react";
 
+const orders = [100, 200, 300]
+
 function App() {
-  const [counter, setCounter] = useState(1)
+  const total = orders.reduce((total, cur) => total + cur)
+  
+  const [counter, setCounter] = useState(total)
+  
   const handleIncrease = () => {
-    setCounter(prevState => prevState + 1)
-    setCounter(prevState => prevState + 1)
     setCounter(prevState => prevState + 1)
   }
   return (
