e4d6215c1af5c78e911d997020bda153e05f5630
nguyen <nguyen@digital-launch.com>
2023-01-03 11:33:58 +0700

    practice react webpack

---
+++ react-webpack/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function App(){
+    return(
+        <div>
+            <h1>Get paid by your work, check final result!!!</h1>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
