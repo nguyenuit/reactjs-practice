1e3bc367974d893b911ca82124dc15ce098a7e8c
nguyen <nguyen@digital-launch.com>
2023-01-05 09:48:04 +0700

    work with radio button

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,14 +1,34 @@
 import { useState } from "react";
 
+const courses = [
+  {
+    id: 1,
+    name: 'JS'
+  },
+  {
+    id: 2,
+    name: 'ReactJS'
+  }
+]
+
 function App(){
-  const [name, setName] = useState('');
+  const [checked, setChecked] = useState(2)
+  const handleSubmit = () => {
+    console.log({id: checked});
+  }
   return (
       <div style={{padding: 20}}>
-        <input
-          value={name} 
-          onChange={e => setName(e.target.value)}
-        />
-        <button onClick={() => setName('test new name')}>Set New Name</button>
+        {courses.map(course => (
+          <div key={course.id}>
+            <input
+              type="radio"
+              checked={checked === course.id}
+              onChange = {() => setChecked(course.id)}
+            />
+            {course.name}
+          </div>
+        ))}
+        <button onClick={handleSubmit}>Register</button>
       </div>
   )
 }
