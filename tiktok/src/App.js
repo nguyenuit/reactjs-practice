17ec7c7b7ff864d8113cc8f3829f6c2b0c07487d
nguyen <nguyen@digital-launch.com>
2023-01-05 09:38:40 +0700

    two-way binding

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,23 +1,14 @@
 import { useState } from "react";
 
-const gifts = [
-  'CPU i9',
-  'RAM 32GB',
-  'RGB Keyboard'
-]
-
 function App(){
-  const [gift, setGift] = useState();
-
-  const randomGift = () => {
-      const index = Math.floor(Math.random() * 3)
-      setGift(gifts[index])
-  }
-
+  const [name, setName] = useState('');
   return (
       <div style={{padding: 20}}>
-          <h1>{gift || 'Chua co phan thuong'}</h1>
-          <button onClick={randomGift}>Lay thuong</button>
+        <input
+          value={name} 
+          onChange={e => setName(e.target.value)}
+        />
+        <button onClick={() => setName('test new name')}>Set New Name</button>
       </div>
   )
 }
