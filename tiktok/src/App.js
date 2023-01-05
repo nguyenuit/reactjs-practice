e1b06b7007b73b89b63c3477d5504eb897c7b17b
nguyen <nguyen@digital-launch.com>
2023-01-05 10:00:31 +0700

    work with checkbox button

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -12,18 +12,34 @@ const courses = [
 ]
 
 function App(){
-  const [checked, setChecked] = useState(2)
+  
+  const [checked, setChecked] = useState([])
+  
+  console.log(checked);
+
+  const handleCheck = (id) => {
+    setChecked(prev => {
+      const isChecked = checked.includes(id)
+      if(isChecked){
+        return checked.filter(item => item !== id)
+      }else{
+        return [...prev, id]
+      }
+    })
+  }
+
   const handleSubmit = () => {
     console.log({id: checked});
   }
+
   return (
       <div style={{padding: 20}}>
         {courses.map(course => (
           <div key={course.id}>
             <input
-              type="radio"
-              checked={checked === course.id}
-              onChange = {() => setChecked(course.id)}
+              type="checkbox"
+              checked={checked.includes(course.id)}
+              onChange = {() => handleCheck(course.id)}
             />
             {course.name}
           </div>
