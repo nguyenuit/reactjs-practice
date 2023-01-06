51abe19fb8ddfd30ddaed50ed0bfd9896c8b9dc3
nguyen <nguyen@digital-launch.com>
2023-01-06 11:16:20 +0700

    useEffect 14

--- tiktok/src/Content.js
+++ tiktok/src/Content.js
@@ -11,70 +11,12 @@ import { useEffect, useState } from "react"
 //-----
 //1. Callback luon duoc goi sau khi component mounted
 
-const tabs = ['posts', 'comments', 'albums']
-
 function Content(){
-    const [title, setTitle] = useState('')
-    const [posts, setPosts] = useState([])
-    const [type, setType] = useState('posts')
-    const [showGoToTop, setShowGoToTop] = useState(false)
-
-    useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/${type}`)
-            .then(res => res.json())
-            .then(posts => {
-                setPosts(posts)
-            })
-    }, [type])
-
-    useEffect(() => {
-        const handleScroll = () => {
-            setShowGoToTop(window.scrollY >= 200)
-        }
-
-        window.addEventListener('scroll', handleScroll)
-        console.log('addEventListener');
-
-        return () => {
-            window.removeEventListener('scroll', handleScroll)
-            console.log('removeEventListener');
-        }
-    }, [])
+    const [width, setWidth] = useState(window.innerWidth)    
     
     return (
         <div>
-            {tabs.map((tab, index) => (
-                <button
-                key={index}
-                style={type === tab ? {
-                    color:'#fff',
-                    backgroundColor: '#333'
-                }:{}}
-                onClick={() => setType(tab)}
-                >
-                    {tab}
-                </button>
-            ))}
-            <input 
-                value={title}
-                onChange={e => setTitle(e.target.value)}
-            />
-            <ul>
-                {posts.map(post => (
-                    <li key={post.id}>{post.title || post.name}</li>
-                ))}
-            </ul>
-            {showGoToTop && (
-                <button
-                    style={{
-                        position: "fixed",
-                        right: 20,
-                        bottom: 20
-                    }}                
-                >
-                    Go To Top
-                </button>
-            )}
+            <h1>{width}</h1>            
         </div>
     )
 }
