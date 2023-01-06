aa613fda5f2052600333584400f641e248a3d539
nguyen <nguyen@digital-launch.com>
2023-01-06 10:00:16 +0700

    useEffect 2

--- tiktok/src/Content.js
+++ tiktok/src/Content.js
@@ -1,19 +1,30 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+
+//1. useEffect(callback)
+//- Goi callback moi khi component re-render
+//- Goi callback sau khi component them element vao DOM
+//2. useEffect(callback, [])
+//3. useEffect(callback, [deps])
+
+//-----
+//1. Callback luon duoc goi sau khi component mounted
 
 function Content(){
+    const [title, setTitle] = useState('')
 
-    //1. useEffect(callback)
-    //2. useEffect(callback, [])
-    //3. useEffect(callback, [deps])
-
-    //-----
-    //1. Callback luon duoc goi sau khi component mounted
     useEffect(() => {
         console.log('Mounted')
     })
 
     return (
-        <h1>Hoc ReactJS!</h1>
+        <div>
+            <h1>Hoc ReactJS!</h1>
+            <input 
+                value={title}
+                onChange={e => setTitle(e.target.value)}
+            />
+            {console.log('Render')}
+        </div>
     )
 }
 
