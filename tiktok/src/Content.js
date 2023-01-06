315c69de18f917bb76263b1e6fffd5b0709f0ea9
nguyen <nguyen@digital-launch.com>
2023-01-06 09:54:16 +0700

    useEffect 1

--- tiktok/src/Content.js
+++ tiktok/src/Content.js
@@ -1,4 +1,17 @@
+import { useEffect } from "react"
+
 function Content(){
+
+    //1. useEffect(callback)
+    //2. useEffect(callback, [])
+    //3. useEffect(callback, [deps])
+
+    //-----
+    //1. Callback luon duoc goi sau khi component mounted
+    useEffect(() => {
+        console.log('Mounted')
+    })
+
     return (
         <h1>Hoc ReactJS!</h1>
     )
