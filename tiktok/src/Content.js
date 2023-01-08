38a4d5c99ab734b1e17280fd622fc64b142d74bb
nguyen <nguyen@digital-launch.com>
2023-01-08 09:31:08 +0700

    useEffect() with preview avatar

--- tiktok/src/Content.js
+++ tiktok/src/Content.js
@@ -13,19 +13,19 @@ import { useEffect, useState } from "react"
 
 function Content(){
     
-    const [countDown, setCountDown] = useState(180)
+    const [count, setCount] = useState(1)
 
     useEffect(() => {
-        const timerId = setInterval(() => {
-            setCountDown(prevState => prevState - 1)
-        }, 1000)
-
-        return () => clearInterval(timerId)
-    }, [])
+        console.log(`Mounted or Re-render lan ${count}`);
+        return () => {
+            console.log(`Cleanup lan ${count}`);
+        }
+    }, [count])
     
     return (
         <div>
-            <h1>{countDown}</h1>            
+            <h1>{count}</h1>
+            <button onClick={() => setCount(count + 1)}>Click me!</button>            
         </div>
     )
 }
