fe020738d46e8acf0508a9c616e9f42ff2d6ab3d
nguyen <nguyen@digital-launch.com>
2023-01-08 11:45:36 +0700

    module js ES6, continue 7

--- js_es6/app.js
+++ js_es6/app.js
@@ -1,6 +1,6 @@
-import logger from './logger/index.js'
+import { logger2 } from './logger/index.js'
 import * as constant from './constant.js'
 
-logger('Get paid by your work!', constant.TYPE_LOG)
-logger('Thuc hanh 1 lan de tranh phai doc lai ly thuyet n lan!', constant.TYPE_WARN)
-logger('Se gap rat nhieu kho khan nhung phai co gang len!', constant.TYPE_WARN)
+logger2('Get paid by your work!', constant.TYPE_LOG)
+logger2('Thuc hanh 1 lan de tranh phai doc lai ly thuyet n lan!', constant.TYPE_WARN)
+logger2('Se gap rat nhieu kho khan nhung phai co gang len!', constant.TYPE_WARN)
--- js_es6/logger/index.js
+++ js_es6/logger/index.js
@@ -1,4 +1,4 @@
-import logger from "./logger.js"
+// import logger from "./logger.js"
 
-export default logger
-// export { default } from "./logger.js"
+// export default logger
+export { default as logger2 } from "./logger.js"
