af3ccb15d5a6ff6fb8c9e28d68eb05f37e21fab0
nguyen <nguyen@digital-launch.com>
2023-01-08 11:13:28 +0700

    module js ES6, continue 1

--- js_es6/app.js
+++ js_es6/app.js
@@ -1,5 +1,8 @@
-import logger from './logger.js'
+import logger, {
+    TYPE_LOG,
+    TYPE_WARN,
+    TYPE_ERROR
+} from './logger.js'
 
-console.log(logger);
-logger('Get paid by your work!', 'warn')
-logger('Se gap rat nhieu kho khan nhung phai co gang len!', 'error')
+logger('Get paid by your work!', TYPE_LOG)
+logger('Se gap rat nhieu kho khan nhung phai co gang len!', TYPE_WARN)
