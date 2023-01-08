f7bb20cb7af6a3c8c20d7b52aad71396a758a85a
nguyen <nguyen@digital-launch.com>
2023-01-08 11:18:16 +0700

    module js ES6, continue 3

--- js_es6/app.js
+++ js_es6/app.js
@@ -1,8 +1,10 @@
-import logger, {
+import logger from './logger.js'
+import {
     TYPE_LOG,
     TYPE_WARN,
     TYPE_ERROR
-} from './logger.js'
+} from './constant.js'
 
 logger('Get paid by your work!', TYPE_LOG)
+logger('Thuc hanh 1 lan de tranh phai doc lai ly thuyet n lan!', TYPE_WARN)
 logger('Se gap rat nhieu kho khan nhung phai co gang len!', TYPE_WARN)
--- js_es6/logger.js
+++ js_es6/logger.js
@@ -1,8 +1,6 @@
-export const TYPE_LOG = 'log'
-export const TYPE_WARN = 'warn'
-export const TYPE_ERROR = 'error'
+import { TYPE_LOG } from "./constant.js"
 
-function logger(log, type='log') {
+function logger(log, type = TYPE_LOG) {
     console[type](log)
 }
 
