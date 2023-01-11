297f1113eab4c15d19f42428245c9c6daec3c11e
nguyen <nguyen@digital-launch.com>
2023-01-11 15:34:58 +0700

    Todo App with useReducer() hook 2

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -9,17 +9,28 @@ const SET_JOB = 'set_job'
 const ADD_JOB = 'add_job'
 const DELETE_JOB = 'delete_job'
 
+const setJob = payload => {
+    return {
+      type: SET_JOB,
+      payload
+    }
+}
+
+console.log(setJob('solve problem!'));
+
 const reducer = (state, action) => {
-  switch(action){
-    case SET_JOB:
-      return state + 1
-    case ADD_JOB:
-      return state - 1
-    case DELETE_JOB:
-      return ''
-    default:
-      throw new Error('Invalid action')
-  }
+  // switch(action){
+  //   case SET_JOB:
+  //     return state + 1
+  //   case ADD_JOB:
+  //     return state - 1
+  //   case DELETE_JOB:
+  //     return ''
+  //   default:
+  //     throw new Error('Invalid action')
+  // }f
+
+  return state;
 }
 
 function App(){
@@ -31,8 +42,11 @@ function App(){
       <div style={{padding: 20}}>
           <h1>Todo</h1>
           <input
-          value={job}
-          placeholder="plan cong viec..."
+            value={job}
+            placeholder="plan cong viec..."
+            onChange={e => {
+              dispatch(setJob(e.target.value))
+            }}
           />
           <button>Add</button>
           <ul>
