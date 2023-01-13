1261aa946644d7f512303c79083e5860a8c14283
nguyen <nguyen@digital-launch.com>
2023-01-13 11:35:22 +0700

    Todo App with useReducer() hook 15

--- tiktok/src/Todo/logger.js
+++ tiktok/src/Todo/logger.js
@@ -3,19 +3,16 @@ import reducer from "./reducer"
 function logger(reducer){
     
     return (prevState, action) => {
-        // console.group(action.type);
-        // console.log(prevState);
+        console.group(action.type);
+        console.log(prevState);
 
-        // const newState = reducer(prevState, action)
-        // console.log(newState);
-        // console.groupEnd();
+        const newState = reducer(prevState, action)
+        console.log(newState);
+        console.groupEnd();
         
-        // return newState
-
-        return reducer(prevState, action)
-
-        // return reducer
+        return newState
     }
 
 }
+
 export default logger
