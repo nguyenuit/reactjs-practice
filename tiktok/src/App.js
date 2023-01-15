9af965583f82292a315483af4f8a171cddf73821
nguyen <nguyen@digital-launch.com>
2023-01-15 10:01:27 +0700

    Global State with Context + useReducer

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,22 +1,8 @@
-import Content from "./Content"
-import { ThemeContext } from "./ThemeContext"
-import { useContext } from "react"
-import "./App.css"
-
-// Context
-// Component A => Component B => Component C
-
-// 1. Create context
-// 2. Provider
-// 3. Consumer
-
 function App(){
-  const context = useContext(ThemeContext)
   return (
-      <div style={{padding: 20}}>
-        <button onClick={context.toggleTheme}>Toggle theme</button>
-        <Content />
-      </div>
+    <div>
+      <h1>Get paid by your work!</h1>
+    </div>
   )
 }
 
