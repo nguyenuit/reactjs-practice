93bf19c193b7f11f50fd45b1a0be20b4f11cacc5
nguyen <nguyen@digital-launch.com>
2023-01-15 10:13:56 +0700

    Global State with Context + useReducer 6

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,4 +1,10 @@
+import { useContext } from "react"
+import { StoreContext } from "./store"
+
 function App(){
+  const state = useContext(StoreContext)
+  console.log(state);
+
   return (
     <div>
       <h1>Get paid by your work!</h1>
