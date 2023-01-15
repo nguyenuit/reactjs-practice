6a740f22a3e1f1415032baf3e32b066ba6bc6cd2
nguyen <nguyen@digital-launch.com>
2023-01-15 10:12:36 +0700

    Global State with Context + useReducer 5

--- tiktok/src/index.js
+++ tiktok/src/index.js
@@ -4,14 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from './ThemeContext';
-
+import { StoreProvider } from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    <ThemeProvider>
+    <StoreProvider>
       <App />
-    </ThemeProvider>
+    </StoreProvider>
   // </React.StrictMode>
 );
 
