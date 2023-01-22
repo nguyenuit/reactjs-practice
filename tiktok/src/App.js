1654e2faefe544ca5756a12f20784dc9dcbc21e5
nguyen <nguyen@digital-launch.com>
2023-01-22 20:59:31 +0700

    css module 3

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,8 +1,14 @@
 import Heading from './components/Heading'
+import Globalstyles from './components/Globalstyles'
 
 function App(){
   return (
-    <Heading />
+    <Globalstyles>
+      <Heading />
+      <div className="d-flex">
+        test
+      </div>
+    </Globalstyles>
   )
 }
 export default App
