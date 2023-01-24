fb46e8c78fe0f187b73a0d12fec2c528877dafb7
nguyen <nguyen@digital-launch.com>
2023-01-24 15:25:29 +0700

    React Router V6

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,8 +1,15 @@
-import Button from "./components/Button"
 
 function App(){
   return (
-    <Button primary disabled />
+    <div className="app">
+      <h1>React Router</h1>
+      <nav>
+        <ul>
+          <li><a href="/">Home page</a></li>
+          <li><a href="/news">News page</a></li>
+        </ul>
+      </nav>
+    </div>
   )
 }
 
