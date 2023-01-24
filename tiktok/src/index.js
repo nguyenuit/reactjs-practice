3db50816fe89dd79dd6ec938d62ff97fbf5b1b44
nguyen <nguyen@digital-launch.com>
2023-01-24 15:31:14 +0700

    React Router V6 2

--- tiktok/src/index.js
+++ tiktok/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,9 +10,11 @@ import { StoreProvider } from './store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    <StoreProvider>
+    // <StoreProvider>
+    <Router>
       <App />
-    </StoreProvider>
+    </Router>
+    // </StoreProvider>
   // </React.StrictMode>
 );
 
